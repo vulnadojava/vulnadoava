be3645d8234b3a8cc8d781eab9f221807b42f136
Sugandasree <107237480+Sugayuv1@users.noreply.github.com>
2024-02-23 20:39:38 +0530

    Update main.tf

--- reverse_shell/main.tf
+++ reverse_shell/main.tf
@@ -2,12 +2,6 @@ provider "aws"  {
   region = "${var.region}"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "${var.vpc_cidr}"
-  tags = {
-    Name = "tmp_vulnado_rev_shell_vpc"
-  }
-}
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = "${aws_vpc.main.id}"
